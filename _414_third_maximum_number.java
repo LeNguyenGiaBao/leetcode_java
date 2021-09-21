028e3f3734b638fe501870884dadc01140c704b6
lenguyengiabao <lenguyengiabao46@gmail.com>
2021-09-21 18:52:09 +0700

    414 and recursion with save memory

--- _414_third_maximum_number.java
+++ _414_third_maximum_number.java
@@ -1,6 +1,32 @@
 public class _414_third_maximum_number {
+    public static void printArr(long[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
     public static void insert(long[] maxArr, int val) {
         int i=0;
+        while(i<maxArr.length) {
+            if (val == maxArr[i]) {
+                return; // not allow 2 elements are the same
+            } else if (val > maxArr[i]) {
+                // insert val. break to get i, insert below
+                break;
+            } else { // val <max[i] --> go to next
+                i++;
+            }
+        }
+        //  insert here
+        if (i <maxArr.length) { // i can over length of maxArr (i = 4) --> val smaller all of maxArr --> not insert 
+            for (int j = 2; j > i; j--) {   // 2 is the last index
+                maxArr[j] = maxArr[j-1];
+            }
+            maxArr[i] = val;
+            printArr(maxArr);
+        }
     }
 
     public static int thirdMax(int[] nums) {
@@ -13,10 +39,11 @@ public class _414_third_maximum_number {
         if (maxArr[2] == Long.MIN_VALUE) {
             return (int)maxArr[0];
         }
-        return (int)maxArr[-1];
+        return (int)maxArr[2];
     }
 
     public static void main(String[] args) {
-        
+        int[] a = {2,2,3,1};
+        System.out.println(thirdMax(a));
     }
 }
--- recursion_fibonaci.java
+++ recursion_fibonaci.java
@@ -1,5 +1,6 @@
 public class recursion_fibonaci {
     public static int fibo(int n) {
+        System.out.println("Calculate F[" + n + "]");
         // fibo(1) = 1 and fibo(2) = 1
         if(n<=2) {
             return 1;
@@ -7,7 +8,25 @@ public class recursion_fibonaci {
         return fibo(n-1) + fibo(n-2);
     }
 
+    // recursion with save variable
+    static int[] F = new int[1000];
+    public static int fibo2(int n) {
+        if(F[n] !=0) {
+            return F[n];
+        }
+        System.out.println("Calculate F[" + n + "]");
+        if(n<=2) {
+            F[1]=1;
+            F[2]=1;
+            return 1;
+        }
+
+        F[n] = fibo2(n-1) + fibo2(n-2);
+        return F[n];
+    }
+
     public static void main(String[] args) {
-        System.out.println(fibo(8));
+        // System.out.println(fibo(10));
+        System.out.println(fibo2(10));
     }
 }
