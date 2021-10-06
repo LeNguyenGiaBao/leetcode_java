96e7725d69b564230fa8558ac9ac84944accec88
lenguyengiabao <lenguyengiabao46@gmail.com>
2021-10-06 21:35:21 +0700

    binary tree

--- _100_same_tree.java
+++ _100_same_tree.java
@@ -13,6 +13,12 @@ public class _100_same_tree {
     }
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
         if (p.val != q.val) {
             return false;
         }
