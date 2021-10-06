6180020b009ef4f6bc94c552f41269a4c50dddbf
lenguyengiabao <lenguyengiabao46@gmail.com>
2021-10-06 08:43:24 +0700

    string

---
+++ _482_license_key_formatting.java
@@ -0,0 +1,28 @@
+public class _482_license_key_formatting {
+    public static String licenseKeyFormatting(String s, int k) {
+        String result = "";
+        char[] ch = s.toCharArray();
+        int n = ch.length;
+        int count = 0;
+        for (int i = n-1; i >= 0; i--) {
+            if(ch[i] != '-') {
+                count++;
+                if (count > k) {
+                    result = "-" + result;
+                    count = 1;
+                }
+
+                result = Character.toUpperCase(ch[i]) + result;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "----a--a----a---a---aaaa--";
+        int k = 2;
+        String result = licenseKeyFormatting(s, k);
+        System.out.println(result);
+        String result2 = "NOTDONE";
+    }
+}
