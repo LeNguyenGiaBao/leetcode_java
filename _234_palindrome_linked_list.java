b71cd3b6ab961ef7d66921847e710900b68460ef
lenguyengiabao <lenguyengiabao46@gmail.com>
2021-10-08 23:16:17 +0700

    linked list, stack

--- _234_palindrome_linked_list.java
+++ _234_palindrome_linked_list.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class _234_palindrome_linked_list {
     public static class ListNode {
         int val;
@@ -76,21 +78,54 @@ public class _234_palindrome_linked_list {
         return true;   
     }
 
+    public static boolean isPalindrome_Stack(ListNode head) {
+        int n = countNode(head);
+        if (n < 2) {
+            return true;
+        }
+
+        int startPop = 0;
+        int endPush = 0;
+        if (n % 2 == 0) {
+            endPush = n/2 - 1;
+            startPop = n/2;
+        } else {
+            endPush = n/2 - 1;
+            startPop = n/2 +1;
+        }
+
+        int curIndex = 0;
+        ListNode curNode = head;
+        Stack<Integer> stack = new Stack<>();
+        while (curNode != null) {
+            if (curIndex <= endPush) {
+                stack.push(curNode.val);
+            }
+
+            if (curIndex >= startPop) {
+                int val = stack.pop();
+                if (val != curNode.val) {
+                    return false;
+                }
+            }
+            curIndex++;
+            curNode = curNode.next;
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
         ListNode n1 = new ListNode(1);
         ListNode n2 = new ListNode(2);
         ListNode n3 = new ListNode(2);
-        ListNode n4 = new ListNode(1);
+        ListNode n4 = new ListNode(4);
 
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
 
-        int count = countNode(n1);
-        System.out.println(count);
-        ListNode reverseNode = reverseListNode(n1);
-        printLinkedList(reverseNode);
-
-        System.out.println(isPalindrome(n1));
+        // System.out.println(isPalindrome(n1));
+        System.out.println(isPalindrome_Stack(n1));
     }
 }
