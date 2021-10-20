f42c99d7d976a1e883e9f55db0e7b2438ea40c8f
lenguyengiabao <lenguyengiabao46@gmail.com>
2021-10-21 01:24:29 +0700

    string stack

---
+++ _151_reverse_words_in_a_string.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class _151_reverse_words_in_a_string {
+    public static String reverseWords(String s) {
+        char[] a = s.toCharArray();
+        int n = a.length;
+        Stack<String> stack = new Stack<>();
+        StringBuilder subString = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] != ' ') {
+                subString.append(a[i]);
+            } else {
+                String word = subString.toString();
+                if (word != "") {
+                    stack.push(word);
+                }
+
+                subString = new StringBuilder();
+            }
+        }
+
+        String result = "";
+        while(!stack.isEmpty()) {
+            String stringPop = stack.pop();
+            result += stringPop + " ";
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "         the         sky        is blue ";
+        String result = reverseWords(s);
+        System.out.println(result);
+    }
+}
