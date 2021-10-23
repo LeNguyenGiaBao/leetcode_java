5602bdd0565f04e4a2d1d2abc342ed912b9ecbed
lenguyengiabao <lenguyengiabao46@gmail.com>
2021-10-23 13:53:15 +0700

    stack string

---
+++ _394_decode_string.java
@@ -0,0 +1,56 @@
+import java.util.Stack;
+import java.io.*;
+
+public class _394_decode_string {
+    public static String decodeString(String s) {
+        int n = s.length();
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
+            if (c == ']') {
+                String str = "";
+
+                // process
+                // loop until meet '['
+                while (stack.peek() != '[') {
+                    str = stack.pop() + str;
+                }
+
+                // remove '['
+                stack.pop();
+
+                // get amount
+                String amount = "";
+                while (stack.isEmpty() == false && Character.isDigit(stack.peek())) {
+                    amount = stack.pop() + amount;
+                }
+                int amountNumber = Integer.parseInt(amount);
+
+                // multiple str amountNumber times
+                // str = str.repeat(amountNumber); // un- co
+
+                // add result "str" back to stack
+                for (int j = 0; j < str.length(); j++) {
+                    stack.push(str.charAt(j));
+                }
+
+            } else {
+                stack.push(c);
+            }
+        }
+
+        String result = "";
+        while (stack.isEmpty() == false) {
+            result = stack.pop() + result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "2[ab2[cd]]";
+        String result = decodeString(s);
+        System.out.println(result);
+    }
+}
