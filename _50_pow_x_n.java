a930cb002c6e1e0dff3c9776f04bd18eff1b115a
lenguyengiabao <lenguyengiabao46@gmail.com>
2021-10-30 22:49:03 +0700

    recursion binary_search

--- _50_pow_x_n.java
+++ _50_pow_x_n.java
@@ -1,9 +1,27 @@
 public class _50_pow_x_n {
-    public double myPow(double x, int n) {
-        return 0;
+    public static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int n_2 = 0;
+        if (n < 0) {
+            n_2 = n % 2 == 0 ? -(n/2) : (-n -1) / 2;
+            x = 1/x;
+        } else {
+            n_2 = n % 2 == 0 ? n/2 : (n - 1) / 2;
+        }
+
+        if (n % 2 == 0) {
+            double t = myPow(x, n_2);
+            return t * t;
+        } else {
+            double t = myPow(x, n_2);
+            return x * t * t;
+        }
     }
 
     public static void main(String[] args) {
-        String a = "Not Done";
+        System.out.println(myPow(2, -2));
     }
 }
