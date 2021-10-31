e2994ecb6024544046c5a5bd4d8d946b2aa32049
lenguyengiabao <lenguyengiabao46@gmail.com>
2021-10-31 18:58:53 +0700

    hash map

--- _387_first_unique_character_in_a_string.java
+++ _387_first_unique_character_in_a_string.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class _387_first_unique_character_in_a_string {
     public static int firstUniqChar(String s) {
         char[] arr = s.toCharArray();   // convert s to char array
@@ -19,9 +22,33 @@ public class _387_first_unique_character_in_a_string {
         return -1;
     }
 
+    public static int firstUniqChar_HashMap(String s) {
+        Map<Character, Integer> myMap = new HashMap<>();
+        char[] a = s.toCharArray();
+
+        for (int i = 0; i < a.length; i++) {
+            if (myMap.containsKey(a[i])) {
+                int value = myMap.get(a[i]);
+                value ++;
+                myMap.put(a[i], value);
+            } else {
+                myMap.put(a[i], 1);
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (myMap.get(a[i]) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         String s = "leetcode";
         int value = firstUniqChar(s);
+        int value2 = firstUniqChar_HashMap(s);
         System.out.println(value);
+        System.out.println(value2);
     }
 }
