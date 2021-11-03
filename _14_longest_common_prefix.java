14b81248dc78340290c75bd5f197d1f0167efae2
lenguyengiabao <lenguyengiabao46@gmail.com>
2021-11-04 01:04:02 +0700

    string

--- _14_longest_common_prefix.java
+++ _14_longest_common_prefix.java
@@ -1,5 +1,21 @@
 public class _14_longest_common_prefix {
-    public String longestCommonPrefix(String[] strs) {
-        return null;
+    public static String longestCommonPrefix(String[] strs) {
+        String temp = strs[0];
+        String result = "";
+        for (int i = 0; i < temp.length(); i++) {
+            String prefix = temp.substring(0, i + 1);
+            for (String s : strs) {
+                if (! s.startsWith(prefix)) {
+                    return result;
+                }
+            }
+            result = prefix;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"dog","racecar","car"};
+        System.out.println(longestCommonPrefix(strs));
     }
 }
